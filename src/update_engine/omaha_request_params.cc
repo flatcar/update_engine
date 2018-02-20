2ab8f597f4d88b308f823ab072d30a73600f232a
Iago López Galeiras <iago@kinvolk.io>
2018-02-20 09:27:51 +0100

    Change update server to Flatcar's

--- src/update_engine/omaha_request_params.cc
+++ src/update_engine/omaha_request_params.cc
@@ -31,7 +31,7 @@ const char* const OmahaRequestParams::kOsPlatform("CoreOS");
 const char* const OmahaRequestParams::kOsVersion("Chateau");
 const char* const OmahaRequestParams::kDefaultChannel("stable");
 const char* const kProductionOmahaUrl(
-    "https://public.update.core-os.net/v1/update/");
+    "https://public.update.flatcar-linux.net/v1/update/");
 
 bool OmahaRequestParams::Init(bool interactive) {
   os_platform_ = OmahaRequestParams::kOsPlatform;
